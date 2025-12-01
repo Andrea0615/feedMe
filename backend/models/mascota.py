94159eb0d6775dfd877ac57e214e9c2f9a976c8f
andrea0615 <A01641193@tec.mx>
2025-12-01 14:03:39 -0600

    add mascota model

---
+++ backend/models/mascota.py
@@ -0,0 +1,12 @@
+from extensions.db import db
+
+class Mascota(db.Model):
+    __tablename__ = "mascota"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    edad = db.Column(db.Integer)
+    raza = db.Column(db.String(50))
+    peso_kg = db.Column(db.Float)
+
+    usuario_id = db.Column(db.Integer, db.ForeignKey("usuario.id"), nullable=False)
