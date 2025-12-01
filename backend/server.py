e8af688fe9b029793b0cf74f187328dc4ae3119f
andrea0615 <A01641193@tec.mx>
2025-11-30 19:04:23 -0600

    add endpoint of login, and validate token

--- backend/server.py
+++ backend/server.py
@@ -2,13 +2,13 @@ from flask import Flask
 from extensions.db import db
 from extensions.cors import cors
 from extensions.jwt import jwt
-
+from config import Config
 # Importar blueprints
 from routes.auth_routes import auth_bp
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("config.Config")
+    app.config.from_object(Config)
 
     db.init_app(app)
     cors.init_app(app)
