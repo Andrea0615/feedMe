5fc5cf39a2f29182d32e6f542263e08e3d898d19
Danna Sánchez <211834102+d-paola-schz@users.noreply.github.com>
2025-12-03 14:16:21 -0600

    Add user_id parameter to event insertion functions
    Updated insertEvent and processIncomingData functions to include user_id parameter for event insertion. Works with sample data, missing connection with physical device, (works only for ultrasonic sensor)

--- helpful_files/insertEvents.py
+++ helpful_files/insertEvents.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from eventos import detect_and_get_events  
 
 
-def insertEvent(event_type, priority, timestamp):
+def insertEvent(event_type, priority, timestamp, user_id):
     engine = create_engine("mariadb+pymysql://ras4:ras4@localhost/mqttesting")
     metadata = MetaData()
     metadata.reflect(bind=engine)
@@ -16,20 +16,37 @@ def insertEvent(event_type, priority, timestamp):
     events_table = metadata.tables["eventos"]
 
     insert_stmt = events_table.insert().values(
-        tipo=event_type,
+        tipo_evento=event_type,
         prioridad=priority,
-        fecha=timestamp
+        fecha=timestamp,
+        id_cuenta = user_id
     )
 
     mdb.execute(insert_stmt)
     mdb.commit()
 
 
-def processIncomingData(json_data):
-    events = detect_and_get_events(json_data)
+def processIncomingData(json_data, user_id):
+    events = detect_and_get_events(json_data, user_id)
 
     for event_type, priority, timestamp in events:
         if event_type != "" and priority > 0:
-            insertEvent(event_type, priority, timestamp)
+            insertEvent(event_type, priority, timestamp, user_id)
 
     return events
+    
+if __name__ == "__main__":
+    json_data_sample = {
+        "hora": "14:07:00",
+        "horario": "14:07:00",
+        "sensores": [
+            {"id": 1, "valor": 2, "unidad": "cm"},
+            {"id": 2, "valor": 150, "unidad": "g"}
+        ]
+    }
+    user_id = 1
+
+    events = processIncomingData(json_data_sample, user_id)
+    print("Eventos detectados:")
+    print(events)
+
